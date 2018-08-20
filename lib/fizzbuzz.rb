19d0b7fb2cafa37f64d56ecda0d1246e704d2fa8
victorjefferies <victor.jefferies@live.com>
2018-08-20 14:20:37 +0000

    finished pair programming exercise - fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,11 @@
 def fizzbuzz(number)
-    'fizz'
+  if (number % 5 == 0) && (number % 3 == 0)
+    "fizzbuzz"
+  elsif (number % 5 == 0)
+    "buzz"
+  elsif (number % 3 == 0)
+    "fizz"
+  else
+    number
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,21 @@
 require 'fizzbuzz'
-
 describe 'fizzbuzz' do
-    it 'returns "fizz" when passed 3' do
-        expect(fizzbuzz(3)).to eq 'fizz'
-    end
+  it 'returns "fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'fizz'
+  end
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns "fizz" when passed 6' do
+    expect(fizzbuzz(6)).to eq 'fizz'
+  end
+  it 'returns "buzz" when passed 10' do
+    expect(fizzbuzz(10)).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns 2 when passed 2' do
+    expect(fizzbuzz(2)).to eq 2
+  end
 end
